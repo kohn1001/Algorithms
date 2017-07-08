74798a2b31ad898182813e1736cac41bda463eb0
K1001 <kohn1001@gmail.com>
2017-07-08 12:55:26 +0300

    mutex pseudo possible implementation - take 1

---
+++ src/my_mutex.cpp
@@ -0,0 +1,40 @@
+
+class MyMutex {
+	public:
+	MyMutex() : mut(0) {}
+	~Mymutex() {}
+
+	void lock();
+	void unlock();
+	bool tryLock();
+
+	private:
+
+	int mut;
+	queue<int> q;
+
+};
+
+void MyMutex::lock()
+{
+	auto threadId = getThreadId();
+	while(!testNset(mut)) {
+		q.push(threadId);
+		markNotRunning(threadId);
+		callSche();
+	}
+}
+
+void MyMutex::unlock()
+{
+	if (!q.empty()) {
+		auto threadId = q.front(); q.pop();
+		markRunning(threadId);
+		callSched();
+		lock();
+	}
+	else {
+		mut = 0;
+	}
+}
+
