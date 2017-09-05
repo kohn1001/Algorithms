f7e10daba5ea0a9b7dac03914b98511b49796f7b
Nathaniel (Natty) Kohn <natty.kohn@intel.com>
2017-09-05 15:23:14 +0300

    Update longest_seq_diff_one.cpp

--- src/longest_seq_diff_one.cpp
+++ src/longest_seq_diff_one.cpp
@@ -3,9 +3,10 @@
 #include <vector>
 #include <map>
 /*
-
+Good Amazon interview question
 Find the logest sequence the objects are differ by one:
-[1,1,1,0,0,2,2,2]
+[1,1,1,0,0,2,2,2] - should return 6
+[0,0,1,1,3,3] - should return 4
 
 */
 using namespace std;
@@ -40,7 +41,10 @@ int getLongestSeq(vector<int> &a)
 int main()
 {
 	vector<int> a = {1, 1, 1, 0, 0, 2, 2, 2,};
+	vector<int> b = {1, 1, 0, 0, 3, 3, 3};
 
 	cout << "longest Subseq is: " << getLongestSeq(a) << endl;
+	cout << "longest Subseq is: " << getLongestSeq(b) << endl;
+
 	return 0;
 }
