3548af448499c9ce701c8e7acc179cf6387d0a81
K1001 <kohn1001@gmail.com>
2017-09-05 19:13:14 +0300

    optimization to hash calculation

---
+++ src/find_anagram_as_substr.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <vector>
+
+using namespace std;
+/*
+ *
+ *
+ * Find Anagram in a string as substring: this is a super cool algorithm I've come with (I'm using a variation of Rabin-Karp Algo)
+ *
+ * 1. Interate on the longer string (in our case str is given as longer than pattern), for in in str:
+ * 2. run substr from i pat.size() bytes, hold it in tmp_str.
+ * 3. calc hash (as describe below) of tmp_str and compare to hash of pattern, if eqaul we have a match
+ *
+ * 
+ * Hash calculation:
+ * Assign for each character from 'a' to 'z' a prime number (do it once as preprocessing and use a lookup table for every calculation)
+ * To get the Hash of a word - get the prime for each char in the word and multiply them, for example:
+ * word = abc
+ * a = 2
+ * b = 3
+ * c = 5
+ * hash = 2*3*5
+ * I'm doing an optimization below - after we calc the first hash from i =0 to i + patt.size(), from now on we can divide by the prime of the i-th char and multiply
+ * with the prime of the char that we now inserting in our window, for example, if str is abcdef, the first hash is 2*3*5, the next hash calc would be:
+ * divid by 2 (a) and multiply by 7 assuming 7 is the prime assigend to d.
+ *
+ */
+
+const int SIZE = 1 << 15;
+const int alphaBetSize = 26;
+const char startWithChar = 'a';
+
+static vector<int> primes (SIZE, 1);
+int getPrime (char c)
+{
+	static bool firstTime = true;
+	static map<char, int> charToPrime;
+	if(firstTime) {
+		int i = 2, cnt = 0;
+		char ic = startWithChar;
+		while (cnt < alphaBetSize) {
+			cout << "i is: " << i << endl;
+			cnt += (primes[i] ? 1 : 0);
+			if(primes[i]) {
+				charToPrime.emplace(ic, i);
+				++ic;
+			}
+			++i;
+		}
+		firstTime = false;
+	}
+	return charToPrime[c];
+}
+
+int calcHash(const string & str, int n = SIZE)
+{
+	int cnt = 1;
+	for (auto s:str) {
+		//cnt *= charToPrime[s];
+		cnt *= getPrime(s);
+	}
+	return cnt;
+}
+
+bool checkAnaAsSubstr(const string &str, const string &s) 
+{
+	int sHash = calcHash(s);
+	string sub = str.substr(0, s.size());
+	int strHash = calcHash(sub);
+	for ( int i=0; i < str.size(); ++i) {
+	//	if (calcHash(sub) == sHash) {
+		if (strHash == sHash) {
+			return true;
+		}
+		strHash /= getPrime(str[i]);
+		strHash *= getPrime(str[i+s.size()]);
+	}
+	return false;
+}
+
+int main()
+{
+	string str = "atcbcba";
+	vector<string> v {"cat", "ccb", "abc", "ztc", "tbc"};;
+	for (auto &s : v) {
+		cout << s << " " <<  (checkAnaAsSubstr(str, s) ? "is Anagram " : "No Anagram ") << endl;
+	}
+	char c = 'a';
+	while (c <= 'z') {
+		cout << "prime for: " << c << " is: " << getPrime(c) << endl;
+		++c;
+	}
+	
+	return 0;
+}
+	
