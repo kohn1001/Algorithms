28307298939fc9208c7cbba03e1afe722a683a4e
K1001 <kohn1001@gmail.com>
2017-09-05 19:47:24 +0300

    added some tests

--- src/break_word_2.cpp
+++ src/break_word_2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 bool isInDict(string str)
 {
-	static unordered_set<string> dict = { "i", "love", "harry", "potter" };
+	static unordered_set<string> dict = { "ilo", "ilove", "love","harry", "potter" };
 	if (dict.find(str) == dict.end()) { return false; }
 	return true;
 }
@@ -16,11 +16,10 @@ bool isInDict(string str)
 
 bool break_word (string str, vector<string> &res)
 {
-//	cout << "string is: " << str << endl;
-	int i;
-	//cin >> i;
-	if (str.empty()) { return true; }
-	for (int i=0; i < str.size(); ++i) {
+	if (str.empty()) { 
+		return true;
+	}
+	for (int i=0; i <= str.size(); ++i) {
 		string new_str = str.substr(0, i);
 		cout << "new_str is : " << new_str << endl;
 		if (isInDict(new_str)) { 
@@ -29,8 +28,8 @@ bool break_word (string str, vector<string> &res)
 				return true;
 			}
 			else {
-//				auto to_del = find(res.begin(), res.end(), new_str);
-//				res.erase(to_del);
+				auto to_del = find(res.begin(), res.end(), new_str);
+				res.erase(to_del);
 			}
 		}
 	}
